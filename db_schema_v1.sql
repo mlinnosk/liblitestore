75558406c886f599971f88d4ffdff179fc8deb92
mlinnosk <markku.linnoskivi@gmail.com>
2013-11-09 10:11:44 +0200

    Initial DB schema

---
+++ db_schema_v1.sql
@@ -0,0 +1,22 @@
+CREATE TABLE meta(
+	   schema_version INTEGER NOT NULL DEFAULT 1
+);
+CREATE TABLE objects(
+	   id INTEGER PRIMARY KEY NOT NULL,
+	   name TEXT NOT NULL,
+	   type INTEGER NOT NULL
+);
+CREATE TABLE object_data(
+	   id INTEGER NOT NULL,
+	   json_key BLOB NOT NULL,
+	   json_value BLOB NOT NULL,
+	   FOREIGN KEY(id) REFERENCES objects(id)
+   	   ON DELETE CASCADE ON UPDATE RESTRICT
+);
+CREATE TABLE array_data(
+	   id INTEGER NOT NULL,
+	   array_index INTEGER NOT NULL,
+	   json_value BLOB NOT NULL,
+	   FOREIGN KEY(id) REFERENCES objects(id)
+   	   ON DELETE CASCADE ON UPDATE RESTRICT
+);
