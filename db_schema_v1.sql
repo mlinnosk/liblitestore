ccb9d27dd7ab85ca4340447f8f7e686bb80e9e9e
Markku Linnoskivi <markku.linnoskivi@gmail.com>
2013-12-28 12:28:01 +0200

    Add 'IF NOT EXISTS' specifier to schema

--- db_schema_v1.sql
+++ db_schema_v1.sql
@@ -1,25 +1,25 @@
-CREATE TABLE meta(
+CREATE TABLE IF NOT EXISTS meta(
        schema_version INTEGER NOT NULL DEFAULT 1
 );
-CREATE TABLE objects(
+CREATE TABLE IF NOT EXISTS objects(
        id INTEGER PRIMARY KEY NOT NULL,
        name TEXT NOT NULL UNIQUE,
        type INTEGER NOT NULL
 );
-CREATE TABLE raw_data(
+CREATE TABLE IF NOT EXISTS raw_data(
        id INTEGER NOT NULL,
        raw_value BLOB NOT NULL,
        FOREIGN KEY(id) REFERENCES objects(id)
        ON DELETE CASCADE ON UPDATE RESTRICT
 );
-CREATE TABLE kv_data(
+CREATE TABLE IF NOT EXISTS kv_data(
        id INTEGER NOT NULL,
        kv_key BLOB NOT NULL,
        kv_value BLOB NOT NULL,
        FOREIGN KEY(id) REFERENCES objects(id)
        ON DELETE CASCADE ON UPDATE RESTRICT
 );
-CREATE TABLE array_data(
+CREATE TABLE IF NOT EXISTS array_data(
        id INTEGER NOT NULL,
        array_index INTEGER NOT NULL,
        array_value BLOB NOT NULL,
